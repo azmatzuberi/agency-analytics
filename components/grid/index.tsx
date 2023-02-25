b568ac77e0a03769a518e0d64863b949b6b9e239
Azmat Zuberi <azmatzuberi@gmail.com>
2023-02-25 14:51:47 -0500

    Tabs initialized

--- components/grid/index.tsx
+++ components/grid/index.tsx
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
 import Card from '@/components/card'
 import { Provider, connect } from 'react-redux'
-// import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { NextPage } from 'next/types';
-// import { wrapper } from '@/redux/store';
 
-const Grid = ({photos}: {photos: Photo[]} ): JSX.Element => {
+const Grid = ({photos}: {photos: Photo[]}, {favorites}: {favorites: Photo[]} ): JSX.Element => {
  
     return (
         <div className="grid">
             <div>
                 <div className='row'>
-                    { photos.map((photo: Photo) => <Card key={photo.id} photo={photo} /> )}
+                    { photos && photos.map((photo: Photo) => <Card key={photo.id} photo={photo} /> )}
+                    { favorites && favorites.map((favorite: Photo) => <Card key={favorite.id} photo={favorite} /> )}
                 </div>
             </div>
         </div>
@@ -25,5 +24,3 @@ function mapStateToProps(state: any) {
 }
 
 export default connect(mapStateToProps)(Grid);
-// export default Grid
-
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,40 @@
+import Grid from '@/components/grid'
+import React from 'react';
 const Header = () => {
 
-    const openTab = (name: string) => {
-
+    const setTab = (tab: string) => {
+        if (tab === 'tab1') {
+            const elem = document.getElementById("label-1") 
+            elem?.classList.add("selected")
+            const elem2 = document.getElementById("label-2")
+            elem2?.classList.remove("selected") 
+        }
+        else {
+            const elem = document.getElementById("label-1") 
+            elem?.classList.remove("selected")
+            const elem2 = document.getElementById("label-2")
+            elem2?.classList.add("selected") 
+        }
     }
 
     return (
     <div className="header">
         <h1>Photos</h1>
-        <div className="tabs">
-            <button className="tablinks" onClick={ () => openTab('Recently added')}>Recently Added</button>
-            <button className="tablinks" onClick={ () => openTab('Favorited')}>Favorited</button>
+        <div className='tabs'>
+            <div className="tab-2">
+                <label id="label-1" className="selected" htmlFor="tab2-1">Recently added</label>
+                <input id="tab2-1" name="tabs-two" type="radio" defaultChecked onClick={() => {setTab("tab1")}} />
+                <div>
+                    <Grid />
+                </div>
+            </div>
+            <div className="tab-2">
+                <label id='label-2' htmlFor="tab2-2">Favorites</label>
+                <input id="tab2-2" name="tabs-two" type="radio" onClick={() => setTab("tab2")} />
+                <div>
+                    <Grid />
+                </div>
+            </div>
         </div>
     </div>
     );
